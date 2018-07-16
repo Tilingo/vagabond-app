aaa6da5ffa03de5599a72355a9d71340a9f930fe
Tilingo <nestirco@gmail.com>
2018-07-16 15:52:45 -0400

    DB created, seeded and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+City.destroy_all
+
+atlanta = City.create(name: 'Atlanta', photo_url: 'https://www.atlantaga.gov/Home/ShowImage?id=3272&t=636335665341170000')
+
+# rihanna.songs.create(title: 'Umbrella', genre: 'Pop')
+atlanta.posts.create(title: 'Amazin city', content: 'Thank you to the City of Atlanta for brining me the greatest happiness over the past 4 years. I came here to attend school at Emory University and fell in love with the city. Atlanta is such a great place, especially for young people. From the Beltline to the many weekend festivals, there is so much to do and explore; so many fun and delicious restaurants. I hope to have the opportunity to live here again. If anyone considers moving to Atlanta, they should not hesitate.')
