4e3c58fb0bf36e9dd7977c91a83bcba2351d3b7d
helioboros <felinewraith@gmail.com>
2018-07-17 14:39:35 -0400

    parallax working

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import HomeParallax from './ParallaxHome';
+import HomeParallax from './ParallaxHome'
 
 class Homepage extends Component {
     render() {
         return (
             <div>
-                horg
                 <HomeParallax />
             </div>
         );
