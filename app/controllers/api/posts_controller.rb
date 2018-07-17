44cccd68da0ec009c9822a9441a7d6712e96d31f
kat2018 <kat.vge@gmail.com>
2018-07-17 12:04:28 -0400

    added posts controlers CRUD

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,38 @@
 class Api::PostsController < ApplicationController
+    
+
+    
+    
+    
+    def index
+        @posts = City.find(params[:city_id]).posts
+        render json: @posts
+    end
+
+    def create
+        @city = City.find(params[:city_id])
+        @post = @city.posts.create(post_params)
+        render json: @post
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render json: @post
+    end
+
+    def update
+        @post = Post.find(params[:city_id]).posts.update(post_params)
+        render json: @posts
+    end
+    
+    # def destroy
+    #     @posts = City.find(params[:id]).destroy
+    #     render status: :ok
+    # end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
