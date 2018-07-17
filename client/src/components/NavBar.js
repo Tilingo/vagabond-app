7ff23d7d37a75b4eb44236a7f21e0ee4a8ad9d8a
helioboros <felinewraith@gmail.com>
2018-07-16 23:02:41 -0400

    copied over default react header styling to navbar; will tweak

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Navbar = styled.div`
+background-color: #222;
+height: 150px;
+padding: 20px;
+color: white;
+`
 
 class NavBar extends Component {
     render() {
         return (
-            <div>
+            <Navbar>
                 Vagabond
-            </div>
+            </Navbar>
         );
     }
 }
