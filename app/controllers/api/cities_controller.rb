05b7996accca44ce42273d30799a94b18763d1e1
Tilingo <nestirco@gmail.com>
2018-07-17 11:20:43 -0400

    Full crud for the City model

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,2 +1,34 @@
 class Api::CitiesController < ApplicationController
+
+    def index
+        @cities = City.all
+        render json: @cities
+    end
+
+    def create
+        @city = City.create!(city_params)
+        render json: @city
+    end
+
+    def show
+        @city = City.find(params[:id])
+        render json: @city
+    end
+
+    def update
+        @city = City.find(params[:id])
+        @city.update!(city_params)
+        render json: @city
+    end
+    
+    def destroy
+        @city = City.find(params[:id]).destroy
+        render status: :ok
+    end
+
+    private
+    def city_params
+        params.require(:city).permit(:name, :photo_url)
+    end
+    
 end
