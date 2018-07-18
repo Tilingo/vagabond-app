2ace4da7c6e34f7cc63a861541b249675ec2537c
Tilingo <nestirco@gmail.com>
2018-07-18 12:13:02 -0400

    foreign key added to post poiting to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_160352) do
+ActiveRecord::Schema.define(version: 2018_07_18_161235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2018_07_18_160352) do
     t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["city_id"], name: "index_posts_on_city_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2018_07_18_160352) do
   end
 
   add_foreign_key "posts", "cities"
+  add_foreign_key "posts", "users"
 end
