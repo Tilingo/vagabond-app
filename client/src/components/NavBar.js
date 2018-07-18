7b237a677e3bd78dfc42f930e8b897fef891a331
helioboros <felinewraith@gmail.com>
2018-07-18 12:52:04 -0400

    re-added navbar styling, beginning user page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Navbar = styled.div`
+    background-color: rgba(38, 23, 66, .33);
+    color: #E9D05A;
+    text-shadow: -1px 0 #261742, 0 1px #261742, 1px 0 #261742, 0 -1px #261742;
+    height: 75px;
+    width: 100%;
+    padding: 10px;
+    border-bottom: 10px solid #F7C749;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: fixed;
+    font-family: 'Fauna One', serif;
+`
+const Title = styled.div`
+    margin: 0 0 0 10px;
+    font-size: 24pt;
+`
+const Navigation = styled.div`
+    margin: 0 10px 0 0;
+    display: flex-row;
+`
+
 
 class NavBar extends Component {
     render() {
         return (
-            <div>
-                Vagabond
-            </div>
+            <Navbar>
+                <Title>Vagabond</Title>
+                <Navigation>
+                    <ul>
+                        <li>Sign up</li>
+                        <li>Log in</li>
+                    </ul>
+                </Navigation>
+            </Navbar>
         );
     }
 }
---
+++ client/src/components/Profile.js
@@ -0,0 +1,16 @@
+// import React, {Component} from 'react'
+// import styled from 'styled-components'
+// import Navbar from '../../node_modules/react-materialize/lib/Navbar';
+
+// class Profile extends Component {
+//     render() {
+//         return (
+//             <Navbar />
+//             <div>
+//                 hewwo?
+//             </div>
+//         )
+//     }
+// }
+
+// export default Profile
